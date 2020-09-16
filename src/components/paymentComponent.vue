c192e1029b81de470946312a8bdb1761195c4525
追风の牧羊人 <30650000+qq3245096941@users.noreply.github.com>
2020-09-16 19:00:02 +0800

    修复bug

--- src/components/paymentComponent.vue
+++ src/components/paymentComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <van-tabs v-if="list.length>0">
+    <van-tabs>
       <van-tab v-for="(item,index) in list" :key="index" :title="item.payName">
         <van-cell-group v-if="item.payType==='2'">
           <van-cell title="姓名" :value="item.realName" />
@@ -11,16 +11,6 @@
         <img v-else :src="imgPrefixUrl+item.payUrl" alt />
       </van-tab>
     </van-tabs>
-
-    <van-empty v-else description="暂无支付方式，请添加支付方式">
-      <van-button
-        size="mini"
-        round
-        type="warning"
-        class="bottom-button"
-        @click="$router.push({path:'/layoutNoTab/gatheringManager'})"
-      >添加支付方式</van-button>
-    </van-empty>
   </div>
 </template>
 
